4c3998e218263f7911d2e4146c3c6f9a5eab90a8
Amolika Bhasin <amolikabhasin@gmail.com>
2022-04-25 10:21:33 +0530

    Time: 4 ms (66.02%), Space: 49.1 MB (28.13%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,43 +1,31 @@
 class Solution {
-	public List<List<Integer>> permuteUnique(int[] nums) {
-		List<List<Integer>> res = new ArrayList<>();
-		Arrays.sort(nums);
-
-		Map<Integer, Integer> map = new HashMap<>();
-		//store the amount for each number
-		for (int num : nums) {
-			map.put(num, map.getOrDefault(num, 0) + 1);
-		}
-		backtrack(res, new ArrayList<>(), map, nums);
-		return res;
-	}
-
-	void backtrack(List<List<Integer>> res, List<Integer> path, Map<Integer, Integer> map, int[] nums) {
-		if (path.size() == nums.length) {
-			res.add(new ArrayList<>(path));
-			return;
-		}
-
-		for (int i = 0; i < nums.length; i++) {
-			final int cur = nums[i];
-			if (i > 0 && cur == nums[i -1]) {
-				continue;
-			}
-
-			// no available number to use
-			if (map.get(cur) <= 0) {
-				continue;
-			}
-
-			//comsume the number
-			map.put(cur, map.get(cur) - 1);
-
-			path.add(cur);
-			backtrack(res, path, map, nums);
-			path.remove(path.size() - 1);
-
-			//revoke use
-			map.put(cur, map.get(cur) + 1);
-		}
-	}
+    List<List<Integer>> ans;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        ans = new LinkedList<>();
+        LinkedList<Integer> path = new LinkedList<>();
+        boolean[] used = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(nums, path, used);
+        return ans;
+    }
+    
+    private void backtrack(int[] nums, LinkedList<Integer> path, boolean[] used) {
+        if (path.size() == nums.length) {
+            ans.add(new LinkedList<>(path));
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = true;
+            backtrack(nums, path, used);
+            used[i] = false;
+            path.removeLast();
+        }
+    }
 }
