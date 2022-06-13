38347b352b86a29a967880d866242d0cd775947a
Amolika Bhasin <amolikabhasin@gmail.com>
2022-06-13 13:27:00 +0530

    Time: 4 ms (95.93%), Space: 42 MB (67.92%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,19 +1,16 @@
-public class Solution {
-    int count = 0;
-    
-    public int findTargetSumWays(int[] nums, int S) {
-        calculate(nums, 0, 0, S);
-        return count;
-    }
-    
-    public void calculate(int[] nums, int i, int sum, int S) {
-        if (i == nums.length) {
-            if (sum == S) {
-                count++;
-            }
-        } else {
-            calculate(nums, i + 1, sum + nums[i], S);
-            calculate(nums, i + 1, sum - nums[i], S);
-        }
-    }
+class Solution {
+public int findTargetSumWays(int[] nums, int S) {
+int sum = 0;
+for (int n: nums) sum += n;
+if (sum < S || (sum - S) % 2 == 1) return 0;
+int target = (sum - S) / 2;
+int[] dp = new int[target + 1];
+dp[0] = 1;
+for (int i = 0; i < nums.length; i++) {
+for (int j = target; j >= nums[i]; j--) {
+dp[j] += dp[j - nums[i]];
+}
+}
+return dp[target];
+}
 }
