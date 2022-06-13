8a91d0e8848ab6122ac7e869f46c24aded43a90e
Amolika Bhasin <amolikabhasin@gmail.com>
2022-06-13 12:01:06 +0530

    Time: 5 ms (49.59%), Space: 41.8 MB (91.95%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=0;j<i+1;j++)
+            {
+                int nextRowElement = triangle.get(i).get(j);
+                int left = triangle.get(i+1).get(j);
+                int right = triangle.get(i+1).get(j+1);
+                
+                int min = Math.min(left,right);
+                
+                triangle.get(i).set(j,nextRowElement+min);
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
