3e01c66cc2805586ff4b91e0780b3d4e452d8634
Amolika Bhasin <amolikabhasin@gmail.com>
2022-06-17 12:58:24 +0530

    Time: 1 ms (73.60%), Space: 41.6 MB (64.60%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumMoves(String s) {
+        int index = 0;
+        int result = 0;
+        
+        while (index < s.length()) {
+            if (s.charAt(index) == 'X') {
+                index = index + 2;
+                result++;
+            }
+            index++;
+        }
+        
+        return result;
+    }
+}
