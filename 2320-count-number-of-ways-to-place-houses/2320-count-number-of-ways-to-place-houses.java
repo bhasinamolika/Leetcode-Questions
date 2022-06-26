cddff35900d2d28f9f666aa1d7bd82b6a1b4fb4e
Amolika Bhasin <amolikabhasin@gmail.com>
2022-06-26 13:26:41 +0530

    Time: 15 ms (100.00%), Space: 46.3 MB (60.00%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
@@ -1,30 +1,19 @@
 class Solution {
-public int countHousePlacements(int n) {
-if(n==1)
-{
-return 4;
-}
-if(n==2)
-{
-return 9;
-}
-if(n==3)
-{
-return 25;
-}
-if(n==4)
-{
-return 64;
-}
-int arr[]=new int[n-1];
-arr[0]=3;
-arr[1]=5;
-for(int i=2;i<n-1;i++)
-{
-arr[i]=(arr[i-1]+arr[i-2])%1000000007;
-
+    
+    private static int MOD = 1000000007;
+    public int countHousePlacements(int n) {
+        if(n==1){
+            return 4;
+        }
+        long[] dp = new long[n];
+        int i;
+        dp[0] = 2;
+        dp[1] = 3;
+        
+        for(i=2;i<n;i++){
+            dp[i] = (dp[i-1] + dp[i-2])%MOD;
+        }
+        
+        return (int)(dp[n-1]%MOD * dp[n-1]%MOD)%MOD;
     }
-    long res=(1l*arr[n-2]*arr[n-2])%1000000007;
-    return (int)res%1000000007;
-}
 }
