8273e3208106761335ef6096057690f9bebc40d4
Amolika Bhasin <amolikabhasin@gmail.com>
2022-07-09 18:40:52 +0530

    Time: 26 ms (89.54%), Space: 56.6 MB (95.28%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -1,16 +1,28 @@
 class Solution {
     public int maxResult(int[] nums, int k) {
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->b[1]-a[1]);// storing index and maxSum uptill that index
-        int max=nums[0];
-        pq.offer(new int[]{0,max});// by default we have the index 0 and maxSum will be the value stored at 0th index of nums
-        for(int i=1;i<nums.length;i++){
-            while((i-pq.peek()[0])>k)
-                pq.poll();
-            max = nums[i] + pq.peek()[1];
-            pq.offer(new int[]{i,max});
-            
-        }
-        return max;
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = nums[0];
         
+        Deque<Integer> dq = new ArrayDeque<>();
+        
+        for (int i = 1; i < len; i++) { 
+            //to determin dp[i] we need dp[i-3] [i-2] [i-1] in the deque, so after i++:
+            //currently in queue: i - 4 i - 3 i - 2 for k = 3
+            
+            //Now we need the deque including i-3, i-2, i-1
+            //1- remove out-ranged idx from queue:
+            if (!dq.isEmpty() && dq.peek() < i - k) dq.poll(); //queue: i - 3, i - 2
+            //2- maintain the deque as mono-decreasing stack:
+            while (!dq.isEmpty() && dp[dq.peekLast()] < dp[i - 1]) {
+                dq.pollLast();    
+            }
+            dq.offerLast(i - 1); //queue: i-3, i-2, i-1
+            
+            //update dp[i]
+            int maxLast = dp[dq.peek()];
+            dp[i] = nums[i] + maxLast;
+        }
+        return dp[len - 1];
     }
 }
