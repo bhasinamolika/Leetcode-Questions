f7ec7264d1638cbe29a22853c91b92d8b3dc3635
Amolika Bhasin <amolikabhasin@gmail.com>
2022-10-11 09:09:12 +0530

    Time: 7 ms (36.32%), Space: 92.3 MB (71.69%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        // start with two largest values, as soon as we find a number bigger than both, while both have been updated, return true.
-        int small = Integer.MAX_VALUE, big = Integer.MAX_VALUE;
-        for (int n : nums) {
-            if (n <= small) { small = n; } // update small if n is smaller than both
-            else if (n <= big) { big = n; } // update big only if greater than small but smaller than big
-            else return true; // return if you find a number bigger than both
+        int min= Integer.MAX_VALUE,mid = Integer.MAX_VALUE;//declared two variable for the middle and minimum or first
+        for(int i =0;i<nums.length;i++)
+        {
+            if(nums[i]<min) //store the min of all in min
+            min=nums[i];
+            if(nums[i]>min&&nums[i]<mid)// ensure that mid is greater than min and less than mid 
+            mid=nums[i];
+            if(nums[i]>mid) // ensuring mid as greatest as mid must be smaller than last one.
+            return true;
         }
-        return false;
+       return false;
     }
 }
