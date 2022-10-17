746a6338af27249356ff2eff12e310bc28a2b71a
Amolika Bhasin <amolikabhasin@gmail.com>
2022-10-18 00:30:54 +0530

    Time: 1 ms (88.72%), Space: 42.2 MB (45.15%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,16 +1,38 @@
+// class Solution {
+//     public boolean checkIfPangram(String sentence) {
+//         int characters[]=new int[26];
+//         boolean flag=true;
+//         for(char ch:sentence.toCharArray()){
+//             characters[ch-'a']++;
+//         }
+//         for(int i=0;i<26;i++){
+//             if(characters[i]==0){
+//                 flag=false;
+//                 break;
+//             }
+//         }
+//         return flag;
+//     }
+// }
+
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        int characters[]=new int[26];
-        boolean flag=true;
-        for(char ch:sentence.toCharArray()){
-            characters[ch-'a']++;
+        // Initialize seen = 0 since we start with no letter.
+        int seen = 0;
+        
+        // Iterate over 'sentence'.
+        for (char currChar : sentence.toCharArray()) {
+            // Map each 'currChar' to its index using its ASCII code.
+            int mappedIndex = currChar - 'a';
+
+            // 'currBit' represents the bit for 'currChar'.
+            int currBit = 1 << mappedIndex;
+
+            // Use 'OR' operation since we only add its bit for once.
+            seen |= currBit;
         }
-        for(int i=0;i<26;i++){
-            if(characters[i]==0){
-                flag=false;
-                break;
-            }
-        }
-        return flag;
+        
+        // Once we finish iterating, check if 'seen' contains 26 bits of 1.
+        return seen == (1 << 26) - 1;
     }
 }
