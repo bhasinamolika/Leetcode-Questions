1c7b0b822014700fbf332d1b760bd73ab1ec0025
Amolika Bhasin <amolikabhasin@gmail.com>
2022-10-31 16:08:31 +0530

    Time: 4 ms (76.75%), Space: 44.2 MB (81.67%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void fun(int ind,int [] candidates, int target,List<List<Integer>> ans, List<Integer> ds){
+        if (target==0){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        if(ind==candidates.length){
+            if(target==0) 
+                ans.add(new ArrayList<>(ds));
+            
+            return;
+        }
+        if(candidates[ind]<=target){
+            ds.add(candidates[ind]);
+            fun(ind,candidates,target-candidates[ind],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+        fun(ind+1,candidates,target,ans,ds);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        fun(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
+}
