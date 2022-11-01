2efdf87fd735cad997f999ced248ed7283b4116c
Amolika Bhasin <amolikabhasin@gmail.com>
2022-11-01 20:06:07 +0530

    Time: 9 ms (28.83%), Space: 54.7 MB (18.80%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,19 +1,24 @@
 class Solution {
     public int[] findBall(int[][] grid) {
         int result[] = new int[grid[0].length];
+        Integer memo[][] = new Integer[grid.length + 1][grid[0].length];
 
-        for (int col = 0; col < grid[0].length; col++) {
-            int currentCol = col;
-            for (int row = 0; row < grid.length; row++) {
-                int nextColumn = currentCol + grid[row][currentCol];
+        for (int row = grid.length; row >= 0; row--) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (row == grid.length) {
+                    memo[row][col] = col;
+                    continue;
+                }
+                int nextColumn = col + grid[row][col];
                 if (nextColumn < 0 ||
                         nextColumn > grid[0].length - 1 ||
-                        grid[row][currentCol] != grid[row][nextColumn]) {
-                    result[col] = -1;
-                    break;
+                        grid[row][col] != grid[row][nextColumn])
+                    memo[row][col] = -1;
+                else
+                    memo[row][col] = memo[row + 1][nextColumn];
+                if (row == 0) {
+                    result[col] = memo[row][col];
                 }
-                result[col] = nextColumn;
-                currentCol = nextColumn;
             }
         }
         return result;
