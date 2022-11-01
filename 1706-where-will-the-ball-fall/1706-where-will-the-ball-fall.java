66c2bf8322053015f67f2f2d88c74005c3051246
Amolika Bhasin <amolikabhasin@gmail.com>
2022-11-01 20:05:39 +0530

    Time: 4 ms (50.06%), Space: 54.1 MB (60.83%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,18 +1,21 @@
 class Solution {
-    private int findBallDropCol(int row,int col,int [][]grid){
-        if(row==grid.length)
-            return col;
-        int nextCol=col+grid[row][col];
-        if(nextCol<0 || nextCol>grid[0].length-1 || grid[row][col]!=grid[row][nextCol]){
-            return -1;
-        }
-        return findBallDropCol(row+1,nextCol,grid);
-    }
     public int[] findBall(int[][] grid) {
-        int res[]=new int[grid[0].length];
-        for(int i=0;i<res.length;i++){
-            res[i]=findBallDropCol(0,i,grid);
+        int result[] = new int[grid[0].length];
+
+        for (int col = 0; col < grid[0].length; col++) {
+            int currentCol = col;
+            for (int row = 0; row < grid.length; row++) {
+                int nextColumn = currentCol + grid[row][currentCol];
+                if (nextColumn < 0 ||
+                        nextColumn > grid[0].length - 1 ||
+                        grid[row][currentCol] != grid[row][nextColumn]) {
+                    result[col] = -1;
+                    break;
+                }
+                result[col] = nextColumn;
+                currentCol = nextColumn;
+            }
         }
-        return res;
+        return result;
     }
 }
