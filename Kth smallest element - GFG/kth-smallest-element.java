18f11dfe6471f8c0b34e65ed322ba52cb2edba2f
Amolika Bhasin <amolikabhasin@gmail.com>
2022-12-24 21:22:50 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.java
+++ Kth smallest element - GFG/kth-smallest-element.java
@@ -37,7 +37,16 @@ class Solution{
     public static int kthSmallest(int[] arr, int l, int r, int k) 
     { 
         //Your code here
-        Arrays.sort(arr);
-        return arr[k-1];
+        // Arrays.sort(arr);
+        // return arr[k-1];
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<arr.length;i++){
+            pq.offer(arr[i]);
+            if(pq.size()>k)
+            {
+                pq.poll();
+            }
+        }
+        return pq.poll();
     } 
 }
